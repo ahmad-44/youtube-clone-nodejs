f43b9907a07fb608da20c2894068deea1b09d8e1
ahmad-44 <ahmadyt291@gmail.com>
2024-09-11 12:33:21 +0500

    Setup fule upload. Multer middleware to upload to local server and cloudinary util to take from local to cloud

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.COUDINARY_CLOUD_NAME,
+  api_key: process.env.COUDINARY_API_KEY,
+  api_secret: process.env.COUDINARY_API_SECRET,
+});
+
+const uploadOncloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //file has been uploaded successfully
+    console.log("File has been uploaded successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation failed
+    return null;
+  }
+};
+
+export { uploadOncloudinary };
